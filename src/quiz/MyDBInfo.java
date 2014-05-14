08dbe3fda6c5a2398dd44759e9462e9e335ca1e7
codePrincess2015 <hramesh@stanford.edu>
2014-05-13 20:56:12 -0700

    Update MyDBInfo.java

--- src/quiz/MyDBInfo.java
+++ src/quiz/MyDBInfo.java
@@ -8,9 +8,9 @@ package quiz;
  */
 public class MyDBInfo {
 	
-	public static final String MYSQL_USERNAME = "ccs108iholmes";
-	public static final String MYSQL_PASSWORD = "jaihoomi";
-	public static final String MYSQL_DATABASE_SERVER = "mysql-user-master.stanford.edu";
-	public static final String MYSQL_DATABASE_NAME = "c_cs108_iholmes";
+	public static final String MYSQL_USERNAME = "censored";
+	public static final String MYSQL_PASSWORD = "censored";
+	public static final String MYSQL_DATABASE_SERVER = "censored";
+	public static final String MYSQL_DATABASE_NAME = "censored";
 
 }
